9ed4e2e431f192c5d79cd1a701321d48a2d4a5ef
Nour <nourchaouch44@gmail.com>
2023-05-10 14:32:45 +0200

    wild-series3

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class ProgramController extends AbstractController
 { 
-    #[Route('/app/', name: 'app_index')]
+    #[Route('/program/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
-            'title' => 'Bienvenue !',
+            'website' => 'Wild Series',
          ]);
     }
 
